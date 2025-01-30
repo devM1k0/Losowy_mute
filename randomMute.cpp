f8f96c9fcb97a349ed11bad041b330ec63cd862f
Miko 3 <sunsetsalisz@gmail.com>
2025-01-30 20:56:45 +0100

    Kod

---
+++ randomMute.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::vector;
+using std::string;
+
+
+int main(){
+    int pech;
+    string plik;
+    string x;
+    int linie = 0;
+    int id = 0;
+    int wylosowany;
+    string czas[6] = {"1 minuta", "5 minut", "10 minut", "1 dzień", "tydzień"};
+    vector<string> ofiary;
+
+    srand(time(NULL));
+
+    cout << "---------- Kto dostanie mute? ----------\n";
+
+    cout << "Ile osób dzisiaj ma pecha?\n";
+    cin >> pech;
+
+    cout << "Podaj nazwe pliku z ofiarami: ";
+    cin >> plik;
+    std::fstream kandydaci(plik);
+    while (getline(kandydaci, x))
+    {
+        ofiary.push_back(x);
+        linie++;
+    }
+
+    for (int i = 1; i <= pech; i++){
+        wylosowany = rand() % linie;
+        id = (rand() % 4) + 1;
+        cout << "Wylosowano: " << ofiary[wylosowany] << " na " <<  czas[id] << '\n';
+        id = 0;
+    }
+
+    cout << "----------------------------------------\n";
+}
